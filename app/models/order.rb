0ae7e7a7eeb3429f8beee57a43703ef00c650148
seiyamiyaoka <seiyamiyaoka@gmail.com>
2017-11-08 23:41:12 +0900

    has_oneの並び替え変更

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_one :order_information
   has_many :order_foods
   has_many :foods, through: :order_foods
-  has_one :order_information
 end
