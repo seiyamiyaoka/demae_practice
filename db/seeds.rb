1e64ca98dde953dcb227a2ebcfe297d015843792
seiyamiyaoka <seiyamiyaoka@gmail.com>
2017-11-10 21:24:44 +0900

    pry追加 csvファイル追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-city = City.create(name: "新宿")
+require 'csv'
+
+cities = []
+if File.exist?("./lib/master_data/13tokyo.csv")
+  CSV.foreach('./lib/master_data/13tokyo.csv', encoding: 'Shift_JIS:UTF-8').each do |row|
+    unless cities.include?(row[9])
+      cities << row[9]
+      city = City.create(name: row[9])
+    end
+  end
+end
+city = City.last
 shop = Shop.create(name: "新宿居酒屋", city_id: city.id)
 genre = Genre.create(name: "中華")
 # genreのインスタンスとしてshopsがあるので最終的にはgenreを保存する必要がある
